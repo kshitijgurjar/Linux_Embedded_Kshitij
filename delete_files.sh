e2f85618d4e069217ad5c5eb64bda46eac134b98
kshitijgurjar <58652185+kshitijgurjar@users.noreply.github.com>
2020-03-31 16:40:10 +0530

    Update delete_files.sh

--- delete_files.sh
+++ delete_files.sh
@@ -1,3 +1,19 @@
+# Author :Kshitij H. Gurjar 
+# Date:31March2020 Time::4:31:50
+
+# This is the second assignment of T2 where a shell script is written to remove the files less than 1024 bytes from the specified directory.
+# Give the arguments as specified in format "./(script_name).sh 1024 /(directory_path)" 
+# e.g. "delete_files.sh 1024 /home/kshitij/delete_file_folder"
+
+
+# to find the files as per condition, "find" command is used which argument of type -file and size as 1k.
+# "echo" is used to print the statements
+
+
+# atlast a 'for' loop runs through all files collected by '$files' variable and removes them using remove "rm" command.
+
+
+# Follwoing is the script
 #!/bin/bash
 
 files=$(find $2 -type f -size -$1k)  #variable with 'find' command 
