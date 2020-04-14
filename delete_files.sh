ab4d41be95d77bd933ba46f83c0c04e63129ebb3
kshitijgurjar <58652185+kshitijgurjar@users.noreply.github.com>
2020-04-14 19:47:54 +0530

    Update delete_files.sh

--- delete_files.sh
+++ delete_files.sh
@@ -16,15 +16,25 @@
 # Follwoing is the script
 #!/bin/bash
 
-files=$(find $2 -type f -size -$1k)  #variable with 'find' command 
-
-echo "Showing all files smaller than $1 bytes in $2\n\n"
-
-
-echo $files             #enlist all files which are less than 1024 bytes
-
-for i in $files ; do    #for loop
-    rm $i
-done
-
-echo "Files succesfully deleted which are below 1k size"
+if [ $1 == "help" ] ;then
+    echo "___________________________________"
+    echo "Help for this program"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "Enter the input as ./file_name.sh (space) 1024(size) (space) directory_path "
+    echo "for e.g ./delete_files.sh 1024 ./home/kshitij/delete_files_folder"
+    echo "Sizes are of units 'bits'"
+    echo "Thank You "
+    echo "___________________________________"
+else
+    if [[ $1 =~ ^[+-]?[0-9]+$ ]]; then       #Checking weather the 'size' is INTEGER or not
+        files=$(find $2 -type f -size -$1k)  #variable with 'find' command 
+        echo "Showing all files smaller than $1 bytes in $2\n\n"
+        echo $files             #enlist all files which are less than 1024 bytes
+        for i in $files ; do    #for loop
+            rm $i
+        done
+        echo "Files succesfully deleted which are below 1k size"
+    else
+    echo "Invalid 'size' input.Enter size as INTEGER e.g 1024" 
+    fi
+fi
